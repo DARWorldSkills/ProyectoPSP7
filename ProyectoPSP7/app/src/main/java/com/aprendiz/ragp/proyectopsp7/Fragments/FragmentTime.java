fe83f1ee98ef0a0a4ee68105e2677f75c2402eab
DavidPyn27 <popayan159@gmail.com>
2018-08-03 12:18:08 -0500

    commitab

--- ProyectoPSP7/app/src/main/java/com/aprendiz/ragp/proyectopsp7/Fragments/FragmentTime.java
+++ ProyectoPSP7/app/src/main/java/com/aprendiz/ragp/proyectopsp7/Fragments/FragmentTime.java
@@ -3,9 +3,12 @@ package com.aprendiz.ragp.proyectopsp7.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.aprendiz.ragp.proyectopsp7.R;
 
@@ -23,8 +26,18 @@ public class FragmentTime extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_time, container, false);
+       View view = inflater.inflate(R.layout.fragment_fragment_time, container, false);
+
+        EditText txtTiempo;
+        Button btnGuardarTiempo;
+        RecyclerView recyclerView;
+
+        txtTiempo = view.findViewById(R.id.txtTiempo);
+        btnGuardarTiempo = view.findViewById(R.id.btnGuardarTime);
+        recyclerView = view.findViewById(R.id.recyTime);
+
+        return view;
     }
+
 
 }
